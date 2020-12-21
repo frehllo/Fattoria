ee8069c62a6752b04a3f52f27a5ea18f1ff1a35f
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2020-12-21 17:57:46 +0100

    Aggiungere i file di progetto.

---
+++ ClassiFattoria/Fattoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassiFattoria
+{
+    public class Fattoria
+    {
+        public List<Animale> Animali { get; private set; }
+
+        public Fattoria() { }
+        public void Adotta(Animale a)
+        {
+            Animali.Add(a);
+        }
+    }
+}
